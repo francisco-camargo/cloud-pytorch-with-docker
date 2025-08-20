1ca704c217722f98990b976972981379a4cd1510
Francisco Camargo <86134108+francisco-camargo@users.noreply.github.com>
2025-08-19 22:04:50 -0500

    Add PyTorch Docker image variable for EC2 instance startup

--- tofu/variables.tf
+++ tofu/variables.tf
@@ -44,3 +44,9 @@ variable "security_group_id" {
   description = "Security group ID for the EC2 instance"
   type        = string
 }
+
+variable "pytorch_image" {
+  description = "PyTorch Docker image to pull on EC2 instance startup"
+  type        = string
+  default     = "pytorch/pytorch:2.1.2-cuda12.1-cudnn8-runtime"
+}
